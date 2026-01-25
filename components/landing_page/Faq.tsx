f162f2ddf8afaf4f93821d2115b050677ddd3e11
David <p5y4@laposte.net>
2026-01-25 11:57:48 +0100

    test proof

--- components/landing_page/Faq.tsx
+++ components/landing_page/Faq.tsx
@@ -13,64 +13,124 @@ function Faq() {
     const secondRef = useRef<HTMLDivElement>(null);
     const thirdRef = useRef<HTMLDivElement>(null);
 
-    useGSAP(() => {
+useGSAP(() => {
+  const cards = gsap.utils.toArray<HTMLElement>(".sticky-card");
+  const cta = sectionRef.current?.querySelector(".faq-cta") as HTMLElement;
 
-        const stickys = document.querySelectorAll(".sticky-card");
+  const questionY = 200;
+  const answerY = 300;
 
-        stickys.forEach((card, index) => {
+  gsap.set(cards, { y: 600, opacity: 0 });
+  gsap.set(cta, { opacity: 0, y: 40 });
 
-            // ---- PINNING ----
-            if (index < stickys.length - 1) {
-                ScrollTrigger.create({
-                    trigger: card,
-                    start: "top 10%",
-                    endTrigger: stickys[index + 1],
-                    end: "top 10%",
-                    scrub: 2,
-                    pin: true,
-                    pinSpacing: false
-                });
-            }
-        });
-    }, { scope: sectionRef });
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: sectionRef.current,
+      start: "top top",
+      end: "+=200%",
+      scrub: 1,
+      pin: true,
+      pinSpacing: true,
+    },
+  });
+
+  const animatePair = (q: HTMLElement, r: HTMLElement, at: number) => {
+    tl.to(q, {
+      y: questionY,
+      opacity: 1,
+      duration: 1,
+      ease: "power3.out",
+    }, at);
+
+    tl.to(r, {
+      y: answerY,
+      opacity: 1,
+      duration: 1,
+      ease: "power3.out",
+    }, at + 0.1);
+  };
+
+  const fadeOutPair = (q: HTMLElement, r: HTMLElement, at: number) => {
+    tl.to([q, r], {
+      opacity: 0,
+      duration: 0.5,
+      ease: "power1.out",
+    }, at);
+  };
+
+  let time = 0;
+
+  for (let i = 0; i < cards.length; i += 2) {
+    const q = cards[i];
+    const r = cards[i + 1];
+
+    animatePair(q, r, time);
+
+    if (i >= 2) {
+      fadeOutPair(cards[i - 2], cards[i - 1], time);
+    }
+
+    time += 1.5;
+  }
+
+  // 🔥 Fade out dernière paire
+  const lastIndex = cards.length - 2;
+  fadeOutPair(cards[lastIndex], cards[lastIndex + 1], time);
+  time += 0.6;
+
+  // 🔥 Apparition finale du CTA
+  tl.to(cta, {
+    opacity: 1,
+    y: questionY,
+    duration: 1,
+    ease: "power3.out",
+  });
+
+}, { scope: sectionRef });
+
+
+
+
+
 
 
     return (
-        <section ref={sectionRef} className="relative bg-black text-white text-center mb-20 py-20">
+        <section ref={sectionRef} className="relative bg-black text-white text-center py-20 mb-64">
             <SectionTitle title='LES DENIERES' span='QESTIONS ?' />
             
-            <div ref={firstRef} className="relative sticky-card mt-20 z-2 bg-red-500 h-24 w-full">
+            <div ref={firstRef} className="absolute sticky-card z-2 bg-red-500 h-24 w-full">
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel ipsum consectetur, aut veritatis totam quae soluta, nihil quas porro, tenetur vero? Nam excepturi reiciendis beatae blanditiis nihil repellat numquam nobis.</p>
             </div>
-            <div ref={secondRef} className="relative sticky-card z-3 bg-orange-500 h-24 w-full">
+            <div ref={secondRef} className="absolute sticky-card z-3 bg-orange-500 h-24 w-full">
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel ipsum consectetur, aut veritatis totam quae soluta, nihil quas porro, tenetur vero? Nam excepturi reiciendis beatae blanditiis nihil repellat numquam nobis.</p>
             </div>
-            <div ref={thirdRef} className="relative sticky-card z-4 bg-green-500 h-24 w-full">
+            <div ref={thirdRef} className="absolute sticky-card z-4 bg-green-500 h-24 w-full">
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel ipsum consectetur, aut veritatis totam quae soluta, nihil quas porro, tenetur vero? Nam excepturi reiciendis beatae blanditiis nihil repellat numquam nobis.</p>
             </div>
-            <div  className="relative sticky-card z-5 bg-blue-500 h-24 w-full">
+            <div  className="absolute sticky-card z-5 bg-blue-500 h-24 w-full">
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel ipsum consectetur, aut veritatis totam quae soluta, nihil quas porro, tenetur vero? Nam excepturi reiciendis beatae blanditiis nihil repellat numquam nobis.</p>
             </div>
-            <div  className="relative sticky-card z-6 bg-gray-500 h-24 w-full">
+            <div  className="absolute sticky-card z-6 bg-gray-500 h-24 w-full">
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel ipsum consectetur, aut veritatis totam quae soluta, nihil quas porro, tenetur vero? Nam excepturi reiciendis beatae blanditiis nihil repellat numquam nobis.</p>
             </div>
-            <div  className="relative sticky-card z-7 bg-pink-500 h-24 w-full">
+            <div  className="absolute sticky-card z-7 bg-pink-500 h-24 w-full">
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel ipsum consectetur, aut veritatis totam quae soluta, nihil quas porro, tenetur vero? Nam excepturi reiciendis beatae blanditiis nihil repellat numquam nobis.</p>
             </div>
-            <div  className="relative sticky-card z-8 bg-yellow-500 h-24 w-full">
+            <div  className="absolute sticky-card z-8 bg-yellow-500 h-24 w-full">
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel ipsum consectetur, aut veritatis totam quae soluta, nihil quas porro, tenetur vero? Nam excepturi reiciendis beatae blanditiis nihil repellat numquam nobis.</p>
             </div>
-            <div  className="relative sticky-card z-9 bg-zinc-500 h-24 w-full">
+            <div  className="absolute sticky-card z-9 bg-zinc-500 h-24 w-full">
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel ipsum consectetur, aut veritatis totam quae soluta, nihil quas porro, tenetur vero? Nam excepturi reiciendis beatae blanditiis nihil repellat numquam nobis.</p>
             </div>
            
-            <div className='flex items-center justify-center h-64 mt-10'>
+            <div className="faq-cta flex flex-col gap-10 items-center justify-center mt-10">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis tempora ab rerum nostrum repellendus eos eligendi molestias, facilis qui similique?</p>
                 <a  
                     href="https://www.google.com/search?uds=AOm0WdE2fekQnsyfYEw8JPYozOKzEik-2elEKZuoMX0RuNnOO2GDIgxodzVnOQyNKeXlFnJ-YKUC7TE4kLm3vqIQWIc_K9GQmAyeD4qnUuL2lsL3durM3BCd536avOHSG36vM61mkI3D&q=G.V.S.3D%20Avis&si=AMgyJEtREmoPL4P1I5IDCfuA8gybfVI2d5Uj7QMwYCZHKDZ-E1JhB0J4OEHu0ExSh8WjvsFDX7W-B4bHUGXrldG4Knsut4hAo43-Re3diVTedAjKfiP_Q40r0ulfUf0zCEy5Boez_ebk&cs=1&hl=fr&sa=X&ved=0CCgQ_4MLahcKEwj4yqq4x_2OAxUAAAAAHQAAAAAQBg&biw=2552&bih=1314&dpr=1"
                     target='_blank'
-                    className='relative overflow-hidden bg-linear-to-br from-orange-600 to-red-600 text-white px-6 py-3 rounded-full font-semibold'
+                    className='relative shrink-0 overflow-hidden bg-linear-to-br from-orange-600 to-red-600 text-white px-6 py-3 rounded-full font-semibold'
                 >
-                    Voir plus d&apos;avis
+                    Appellez nous
                     <div  className="absolute inset-0 bg-linear-to-r from-transparent via-white/50 to-transparent skew-x-12 -translate-x-100 will-change-transform" />
                 </a>
             </div>
@@ -78,4 +138,4 @@ function Faq() {
     )
 }
 
-export default Faq
+export default Faq;
