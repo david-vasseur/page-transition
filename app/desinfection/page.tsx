c97dedb0a90eba2a533ebfa9a154ed9043ebe89d
David <p5y4@laposte.net>
2026-01-31 18:44:44 +0100

    test mobile service

--- app/desinfection/page.tsx
+++ app/desinfection/page.tsx
@@ -52,8 +52,6 @@ function page() {
   const [i3_test3, setI3_test3] = useState(50);
   const [i3_angle, setI3_angle] = useState(90);
 
-  // Pin removal flag
-  const [pinned, setPinned] = useState(true);
 
   useGSAP(() => {
     const tl = gsap.timeline({
@@ -64,9 +62,15 @@ function page() {
         scrub: true,
         pin: true,
         pinSpacing: true,
-        onLeave: () => setPinned(false), 
       },
     });
+
+    const title = gsap.utils.toArray(".content-title") as HTMLElement[];
+    if (title.length > 2) {
+        gsap.set(title[1], {opacity: 0, y: 100}); 
+        gsap.set(title[2], {opacity: 0, y: 100}); 
+    }
+       
 
     // Etape 1 → Image1
     tl.to({}, {
@@ -82,6 +86,8 @@ function page() {
         setI1_angle(90 + p * 30);
       }
     });
+    tl.to(title[0], { opacity: 0, yPercent: -150, scale: 0.5, duration: 0.1 }, "<")
+        .to(title[1], { opacity: 1, y: 0, duration: 0.1 }, "<+=0.1")
 
     // Etape 2 → Image2
     tl.to({}, {
@@ -97,6 +103,8 @@ function page() {
         setI2_angle(90 + p * 30);
       }
     });
+    tl.to(title[1], { opacity: 0, yPercent: -150, scale: 0.5, duration: 0.1 }, "<")
+        .to(title[2], { opacity: 1, y: 0, duration: 0.1 }, "<+=0.1")
 
     // Etape 3 → Image3
     tl.to({}, {
@@ -119,49 +127,54 @@ function page() {
     return (
         <div>
             <section className="h-dvh bg-red-400 flex items-center justify-center text-5xl font-black">HERO</section>
-                  <section ref={sectionRef} className="h-lvh relative">
+            <section ref={sectionRef} className="h-lvh relative">
                 <div className="absolute inset-0 z-3">
-                {/* Image1 */}
-                <Image
-                    fill
-                    src="/infestation-dark.png"
-                    alt="gg"
-                    className="z-3 object-cover"
-                    style={{
-                    maskImage: `linear-gradient(${i1_angle}deg, black 0%, black ${i1_test}%, transparent ${i1_test1}%, transparent ${i1_test2}%, black ${i1_test3}%, black 100%)`,
-                    WebkitMaskImage: `linear-gradient(${i1_angle}deg, black 0%, black ${i1_test}%, transparent ${i1_test1}%, transparent ${i1_test2}%, black ${i1_test3}%, black 100%)`,
-                    maskRepeat: "no-repeat",
-                    maskSize: "100% 100%",
-                    }}
-                />
+                    {/* Image1 */}
+                    <Image
+                        fill
+                        src="/infestation-dark.png"
+                        alt="gg"
+                        className="z-3 object-cover"
+                        style={{
+                        maskImage: `linear-gradient(${i1_angle}deg, black 0%, black ${i1_test}%, transparent ${i1_test1}%, transparent ${i1_test2}%, black ${i1_test3}%, black 100%)`,
+                        WebkitMaskImage: `linear-gradient(${i1_angle}deg, black 0%, black ${i1_test}%, transparent ${i1_test1}%, transparent ${i1_test2}%, black ${i1_test3}%, black 100%)`,
+                        maskRepeat: "no-repeat",
+                        maskSize: "100% 100%",
+                        }}
+                    />
 
-                {/* Image2 */}
-                <Image
-                    fill
-                    src="/close-up-pest.png"
-                    alt="gg"
-                    className="z-2 object-cover"
-                    style={{
-                    maskImage: `linear-gradient(${i2_angle}deg, black 0%, black ${i2_test}%, transparent ${i2_test1}%, transparent ${i2_test2}%, black ${i2_test3}%, black 100%)`,
-                    WebkitMaskImage: `linear-gradient(${i2_angle}deg, black 0%, black ${i2_test}%, transparent ${i2_test1}%, transparent ${i2_test2}%, black ${i2_test3}%, black 100%)`,
-                    maskRepeat: "no-repeat",
-                    maskSize: "100% 100%",
-                    }}
-                />
+                    {/* Image2 */}
+                    <Image
+                        fill
+                        src="/close-up-pest.png"
+                        alt="gg"
+                        className="z-2 object-cover"
+                        style={{
+                        maskImage: `linear-gradient(${i2_angle}deg, black 0%, black ${i2_test}%, transparent ${i2_test1}%, transparent ${i2_test2}%, black ${i2_test3}%, black 100%)`,
+                        WebkitMaskImage: `linear-gradient(${i2_angle}deg, black 0%, black ${i2_test}%, transparent ${i2_test1}%, transparent ${i2_test2}%, black ${i2_test3}%, black 100%)`,
+                        maskRepeat: "no-repeat",
+                        maskSize: "100% 100%",
+                        }}
+                    />
 
-                {/* Image3 */}
-                <Image
-                    fill
-                    src="/security-professional.png"
-                    alt="gg"
-                    className="z-1 object-cover"
-                    style={{
-                    maskImage: `linear-gradient(${i3_angle}deg, black 0%, black ${i3_test}%, transparent ${i3_test1}%, transparent ${i3_test2}%, black ${i3_test3}%, black 100%)`,
-                    WebkitMaskImage: `linear-gradient(${i3_angle}deg, black 0%, black ${i3_test}%, transparent ${i3_test1}%, transparent ${i3_test2}%, black ${i3_test3}%, black 100%)`,
-                    maskRepeat: "no-repeat",
-                    maskSize: "100% 100%",
-                    }}
-                />
+                    {/* Image3 */}
+                    <Image
+                        fill
+                        src="/security-professional.png"
+                        alt="gg"
+                        className="z-1 object-cover"
+                        style={{
+                        maskImage: `linear-gradient(${i3_angle}deg, black 0%, black ${i3_test}%, transparent ${i3_test1}%, transparent ${i3_test2}%, black ${i3_test3}%, black 100%)`,
+                        WebkitMaskImage: `linear-gradient(${i3_angle}deg, black 0%, black ${i3_test}%, transparent ${i3_test1}%, transparent ${i3_test2}%, black ${i3_test3}%, black 100%)`,
+                        maskRepeat: "no-repeat",
+                        maskSize: "100% 100%",
+                        }}
+                    />
+                </div>
+                <div className="content absolute flex items-center justify-center text-4xl font-black inset-0 z-3">
+                    <h2 className="content-title absolute z-3">PROBLEME</h2>
+                    <h2 className="content-title absolute z-2">SOLUTION</h2>
+                    <h2 className="content-title absolute z-1">BENEFICE</h2>
                 </div>
             </section>
             <section className="h-dvh bg-red-400 flex items-center justify-center text-5xl font-black">END</section>
