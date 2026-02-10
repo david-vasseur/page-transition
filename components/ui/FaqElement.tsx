8200574d722633011ac0fecf1536bb184f7d5d73
David Vasseur <p5y4@macbook-pro-de-david.home>
2026-02-10 17:38:51 +0100

    correction title

--- components/ui/FaqElement.tsx
+++ components/ui/FaqElement.tsx
@@ -12,7 +12,7 @@ function FaqElement({ icon, label }: IFaqElement) {
 
     return (
         <div  className="absolute flex justify-center items-center sticky-card z-5 h-24 w-full">
-            <div className={`w-[90%] lg:max-w-2xl flex gap-8 items-center justify-evenly p-8 rounded-2xl ${Icon ? "border border-gray-500/60 bg-linear-to-br from-orange-950/60 to-red-800/60" : ""}`}>
+            <div className={`w-[90%] lg:max-w-xl flex gap-8 items-center justify-evenly p-8 rounded-2xl ${Icon ? "border border-gray-500/60 bg-linear-to-br from-orange-950/60 to-red-800/60" : ""}`}>
                 {Icon && 
                 <div className="bg-linear-to-br from-orange-600 to-red-800 p-4 rounded-xl">
                     <Icon className="text-gray-300 w-6 h-6 shrink-0" />
