9e98a0f5100c4a872ab35387ae08187dffa06448
David <p5y4@laposte.net>
2026-02-23 19:52:29 +0100

    final version

--- components/ui/Email.tsx
+++ components/ui/Email.tsx
@@ -3,31 +3,43 @@ import IEstimate from "@/type/estimate"
 
 
 function MyEmail(value: IEstimate) {
-  return (
-    <div style={{ backgroundColor: 'black', color: 'orange', padding: '24px', borderRadius: '8px' }}>
-      <h1>Bonjour !</h1>
-      <h2>{value.name} a fait une demande de devis</h2>
-      <p>{value.message}</p>
+    return (
+        <>
+            <div style={{ backgroundColor: 'black', color: 'orange', padding: '24px', borderRadius: '8px' }}>
+                <h1>Bonjour !</h1>
+                <h2>
+                    <span className="text-blue-600 font-black">{value.name}</span> a fait une demande de devis pour {value.service}</h2>
+                <p>Message: " {value.message} "</p>
 
-      {value.whatsapp && value.phoneNumber && (
-        <a
-          href={`https://wa.me/33${value.phoneNumber.replace(/^0/, '')}`}
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{
-            display: 'inline-block',
-            padding: '12px 24px',
-            backgroundColor: 'orange',
-            color: 'black',
-            fontWeight: 'bold',
-            borderRadius: '8px',
-          }}
-        >
-          Cliquez ici pour discuter sur WhatsApp
-        </a>
-      )}
-    </div>
-  );
+                {value.whatsapp && value.phoneNumber && (
+                    <a
+                        href={`https://wa.me/33${value.phoneNumber.replace(/^0/, '')}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{
+                            display: 'inline-block',
+                            padding: '12px 24px',
+                            backgroundColor: 'orange',
+                            color: 'black',
+                            fontWeight: 'bold',
+                            borderRadius: '8px',
+                        }}
+                    >
+                        Cliquez ici pour discuter sur WhatsApp
+                    </a>
+                )}
+            </div>
+            <div style={{ marginTop: '16px', textAlign: 'center' }}>
+                <img
+                    src="https://gvs3d.fr/logo.webp"
+                    width={600}
+                    height={400}
+                    alt="Logo"
+                    style={{ display: 'block', margin: '0 auto' }}
+                />
+            </div>
+        </>
+    );
 }
 
-export default MyEmail
+export default MyEmail;
